818b0ca70e55f47e1f153f7071639db5f45591a2
SeminKang <ksm9802@gmail.com>
2022-12-04 19:02:44 +0900

    회원가입 후 프로필 설정 페이지

---
+++ src/App copy.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ProfileSetSignUp from "./pages/ProfileSetSignUp/ProfileSetSignUp.jsx";
+export default function App() {
+    return (
+        <ProfileSetSignUp/>
+    );
+}
