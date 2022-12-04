66c0a71864fcef6ea428a898188980eb98c0fa5c
SeminKang <ksm9802@gmail.com>
2022-12-04 20:06:42 +0900

    이름 수정

--- src/components/ProfileSetInpsTemp/ProfileSetInpsTemp.jsx
+++ src/components/ProfileSetInpsTemp/ProfileSetInpsTemp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfileImageSet from "../shared/ProfileImageSet/ProfileImageSet";
-import InputAndWarn from "../shared/InfoInput/InfoInput";
+import InfoInput from "../shared/InfoInput/InfoInput";
 import Warning from "../shared/Warning/Warning";
 import "./profileSetInpsTemp.css"
 import { useState } from "react";
@@ -30,7 +30,7 @@ export default function ProfileSetInpsTemp({ formId }) {
     return (
         <form id={formId}>
             <ProfileImageSet />
-            <InputAndWarn
+            <InfoInput
                 type={"text"}
                 label={"사용자 이름"}
                 placeHolder={"2~10자 이내여야 합니다."}
@@ -44,7 +44,7 @@ export default function ProfileSetInpsTemp({ formId }) {
                 message={"경고"}
                 visible={visible1}
             />
-            <InputAndWarn
+            <InfoInput
                 type={"text"}
                 label={"계정 ID"}
                 placeHolder={"영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."}
@@ -56,7 +56,7 @@ export default function ProfileSetInpsTemp({ formId }) {
                 message={"영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다."}
                 visible={visible2}
             />
-            <InputAndWarn
+            <InfoInput
                 type={"text"}
                 label={"소개"}
                 placeHolder={"자신과 판매할 상품에 대해 소개해 주세요!"}
