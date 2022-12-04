ce01ac70a61c4d2edc17e2d271052b32f223fa14
SeminKang <ksm9802@gmail.com>
2022-12-04 19:37:40 +0900

    파일 업로드시 이미지 바꾸기

--- src/components/shared/ProfileImageSet/ProfileImageSet.jsx
+++ src/components/shared/ProfileImageSet/ProfileImageSet.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useRef} from "react";
 
+// https://developer-talk.tistory.com/330
 export default function ProfileImageSet() {
-    const imgFile = "basic-profile-img.png";
+    const image = useRef(null);
+
+    const handleFiles = (e) => {
+        console.log(e.target.files);
+        const selectedFile = [...e.target.files];
+        const fileReader = new FileReader();
+
+        fileReader.readAsDataURL(selectedFile[0]);
+
+        fileReader.onload = function () {
+            image.current.src = fileReader.result;
+        }
+    }
     return (
         <label>
             <span>프로필 사진 이미지 설정</span>
-            <img src={require(`../../../assets/${imgFile}`)} alt="" />
-            <input type="file" accept="image/*" />
+            <img src={require(`../../../assets/basic-profile-img.png`)} alt="" ref={image} />
+            <input 
+                type="file" 
+                accept="image/*"
+                onChange={handleFiles}
+            />
         </label>
     );
 }
