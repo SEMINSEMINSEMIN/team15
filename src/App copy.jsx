9eebb6d954cb10c8499e5eae47ee1b95fad1f021
SeminKang <ksm9802@gmail.com>
2022-12-04 20:45:28 +0900

    필요없는 파일 삭제

--- src/App copy.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfileSetSignUp from "./pages/ProfileSetSignUp/ProfileSetSignUp.jsx";
-import ProfileSetLogin from "./pages/ProfileSetLogIn/ProfileSetLogin.jsx";
-export default function App() {
-    return (
-        <ProfileSetLogin/>
-    );
-}
