d954bad1621e9da440211bc6711a04f27fe877ab
SeminKang <ksm9802@gmail.com>
2022-12-04 19:11:59 +0900

    로그인 된 상태에서 프로필 설정 페이지

--- src/App copy.jsx
+++ src/App copy.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ProfileSetSignUp from "./pages/ProfileSetSignUp/ProfileSetSignUp.jsx";
+import ProfileSetLogin from "./pages/ProfileSetLogIn/ProfileSetLogin.jsx";
 export default function App() {
     return (
-        <ProfileSetSignUp/>
+        <ProfileSetLogin/>
     );
 }
