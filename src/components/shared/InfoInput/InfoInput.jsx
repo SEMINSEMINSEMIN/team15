fe4750790d5c76b8d2fbe95624e907faee52620f
강세민 <ksm9802@gmail.com>
2022-12-06 17:56:10 +0900

    InfoInput 코드 가독성 개선

--- src/components/shared/InfoInput/InfoInput.jsx
+++ src/components/shared/InfoInput/InfoInput.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
+import React from "react";
 
 export default function InputAndWarn({
-    type, 
-    placeHolder, 
-    pattern, 
+    type,
+    placeHolder,
+    pattern,
     label,
-    required, 
-    minlength, 
+    required,
+    minlength,
     maxlength,
-    onBlur
+    onBlur,
 }) {
+    let inpEl = (
+        <input
+            type={type}
+            placeholder={placeHolder}
+            pattern={pattern}
+            minLength={minlength}
+            maxLength={maxlength}
+            onBlur={onBlur}
+            onFocus={(e) => {
+                e.target.style.backgroundColor = "red";
+            }}
+        />
+    );
+
     if (required) {
-        return (
-            <label>
-                <span>{label}</span>
-                <input
-                    type={type}
-                    placeholder={placeHolder}
-                    pattern={pattern}
-                    minLength={minlength}
-                    maxLength={maxlength}
-                    onBlur={onBlur}
-                    onFocus={(e) => {e.target.style.backgroundColor="red"}}
-                    required
-                />
-            </label>
-        )
-    } else {
-        return (
-            <label>
-                <span>{label}</span>
-                <input
-                    type={type}
-                    placeholder={placeHolder}
-                    pattern={pattern}
-                    minLength={minlength}
-                    maxLength={maxlength}
-                    onBlur={onBlur}
-                    onFocus={(e) => {e.target.style.backgroundColor="red"}}
-                />
-            </label>
-        )       
+        inpEl = (
+            <input
+                type={type}
+                placeholder={placeHolder}
+                pattern={pattern}
+                minLength={minlength}
+                maxLength={maxlength}
+                onBlur={onBlur}
+                onFocus={(e) => {
+                    e.target.style.backgroundColor = "red";
+                }}
+                required
+            />
+        );
     }
+
+    return (
+        <label>
+            <span>{label}</span>
+            {inpEl}
+        </label>
+    );
 }
